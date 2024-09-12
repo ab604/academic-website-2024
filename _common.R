821e56f9f85ad20cf7336b8132c2b52e207216ec
ab604 <ab604@soton.ac.uk>
2024-09-12 18:20:30 +0100

    fix pubs

--- _common.R
+++ _common.R
@@ -89,11 +89,27 @@ make_pub <- function(pub, index = NULL) {
     <div class="grid">
     <div class="g-col-11"> {markdown_to_html(cite)} </div>
     </div>
-    </div>
     {icons}
     </div>'
   )))
 }
+
+aside <- function(text) {
+  return(tag("aside", list(text)))
+}
+
+center <- function(text) {
+  return(tag("center", list(text)))
+}
+
+aside_center <- function(text) {
+  return(aside(center(list(text))))
+}
+
+aside_center_b <- function(text) {
+  return(aside(center(list(tag("b", text)))))
+}
+
 
 markdown_to_html <- function(text) {
   if (is.null(text)) { return(text) }
